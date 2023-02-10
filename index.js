f4be99ca385633afa4c21dd69cfd807180267e74
MASOOD AKHTAR VAHEED <masoodakhtarvaheed@MASOODs-MacBook-Air.local>
2023-02-10 13:52:00 +0530

    added-bodyParser-mongoose-morgan

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+require('dotenv/config')
 
-const PORT = 35000
+const PORT = process.env.PORT
+const api = process.env.API_URL
 
-app.use('/',(req,res)=>{
-    res.send('route working')
-    
+//middleware
+app.use(bodyParser.json())
+app.use(morgan('tiny'))
+
+app.get(`${api}/products`,(req,res)=>{
+    const product = {
+        id:'1',
+        name:'Hair Dryer',
+        image:'some_url'
+    }
+    res.send(product)
+})
+
+app.post(`${api}/products`,(req,res)=>{
+    const newProduct = req.body;
+    console.log(newProduct,'newProduct');
+    res.send(newProduct)
 })
 
 app.listen(PORT,()=>{
