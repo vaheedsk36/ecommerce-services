44dcf66370c11e54e100613f93bcf4d3123ad921
MASOOD AKHTAR VAHEED <masoodakhtarvaheed@MASOODs-MacBook-Air.local>
2023-02-11 17:05:07 +0530

    added-products-schema

--- models/product.js
+++ models/product.js
@@ -6,10 +6,53 @@ const productSchema = new Schema({
         type:String,
         required:true
     },
-    image:String,
+    description:{
+        type:String,
+        required:true
+    },
+    richDescription:{    
+        type:String,
+        default:''
+    },
+    image:{
+        type:String,
+        default:''
+    },
+    images:[{type:String}],
+    brand:{
+        type:String,
+        default:''
+    },
+    price:{
+        type:Number,
+        default:0
+    },
+    category:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Category',
+        required:true
+    },
     countInStock:{
         type:Number,
-        required:true
+        required:true,
+        min:0,
+        max:255
+    },
+    ratings:{
+        type:Number,
+        default:0
+    },
+    numReviews:{
+        type:Number,
+        default:0
+    },
+    isFeatured:{
+        type:Boolean,
+        default:false
+    },
+    dateCreated:{
+        type:Date,
+        default:Date.now
     }
 })
 module.exports = mongoose.model('Product',productSchema);
