353b02992554c76eaadf8b3d97cecfb8b95ea8d4
MASOOD AKHTAR VAHEED <masoodakhtarvaheed@MASOODs-MacBook-Air.local>
2023-02-11 21:45:46 +0530

    added-id-to-products-route

--- routers/product.js
+++ routers/product.js
@@ -3,26 +3,50 @@ const router = express.Router();
 const Product = require('../models/product');
 
 router.get('/',async (req,res)=>{
-    const productsList = await Product.find();
+    const productsList = await Product.find().select('name image -_id');
     if(!productsList){
         res.status(500).json({success:false})
     }
     res.send(productsList)
 })
 
-router.post('/',(req,res)=>{
-    const product = new Product({
-        name:req.body.name,
-        image:req.body.image,
-        countInStock:req.body.countInStock
-    });
-    product.save().then((createdProduct=>{
-        res.status(201).json(createdProduct);
-    })).catch((err)=>res.status(500).json({
-        error:err,
-        status:false
-    }));
-    // res.send(product)
+router.get('/:id',async (req,res)=>{
+    try{
+        const productsList = await Product.findById(req.params.id);
+        if(!productsList){
+            res.status(400).json({success:false})
+        }
+        res.send(productsList)
+    }catch(err){
+        res.status(500).json({
+            error:err,
+            status:false
+        })
+    }
+})
+
+router.post('/',async (req,res)=>{
+    try{
+        let product = new Product({
+            name:req.body.name,
+            image:req.body.image,
+            countInStock:req.body.countInStock
+        });
+        product = await product.save();
+        if(!product){
+            res.status(400).json({
+                error:err,
+                status:false
+            })
+        }
+        res.status(201).json(product);
+
+    }catch(err){
+        res.status(500).json({
+            error:err,
+            status:false
+        })
+    }
 })
 
 module.exports = router;
