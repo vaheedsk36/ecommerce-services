c5a5be237106c21f819a9874dc4baef4c4514bd4
MASOOD AKHTAR VAHEED <masoodakhtarvaheed@MASOODs-MacBook-Air.local>
2023-02-12 13:28:14 +0530

    added-new-route-in-products

--- routers/product.js
+++ routers/product.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/product');
+const mongoose = require('mongoose');
 
 router.get('/',async (req,res)=>{
-    const productsList = await Product.find().select('name image -_id');
+    const productsList = await Product.find().populate('category');
+    if(!productsList){
+        res.status(500).json({success:false})
+    }
+    res.send(productsList)
+})
+
+router.get('/get/count',async (req,res)=>{
+    const productsCount = await Product.countDocuments();
+    if(!productsCount){
+        res.status(500).json({success:false})
+    }
+    res.send({productsCount:productsCount})
+})
+
+router.get('/get/featured',async (req,res)=>{
+    const productsList = await Product.find({isFeatured:true});
     if(!productsList){
         res.status(500).json({success:false})
     }
@@ -12,6 +29,9 @@ router.get('/',async (req,res)=>{
 
 router.get('/:id',async (req,res)=>{
     try{
+        if(!mongoose.isValidObjectId(req.params.id)){
+            return res.status(400).send('Invalid product id');
+        }
         const productsList = await Product.findById(req.params.id);
         if(!productsList){
             res.status(400).json({success:false})
@@ -45,6 +65,62 @@ router.post('/',async (req,res)=>{
         res.status(500).json({
             error:err,
             status:false
+        })
+    }
+})
+
+
+router.put('/:id',async(req,res)=>{
+    try{
+        if(!mongoose.isValidObjectId(req.params.id)){
+            return res.status(400).send('Invalid product id');
+        }
+        let product = await Product.findByIdAndUpdate(req.params.id,{
+            name:req.body.name,
+            image:req.body.image,
+            countInStock:req.body.countInStock
+        },
+        {
+            new:true
+        }
+        );
+        if(!product){
+            res.status(400).json({
+                status:false,
+                message:'Unable to update the product '
+            })
+        }
+        res.send(product)
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            err:err
+        })
+    }
+})
+
+router.delete('/:id',async(req,res)=>{
+
+    try{
+        if(!mongoose.isValidObjectId(req.params.id)){
+            return res.status(400).send('Invalid product id');
+        }
+        let product = await Product.findByIdAndRemove(req.params.id);
+        if(product){
+            return res.status(200).json({
+                success:true,
+                message:'Product has been deleted'
+            })
+        }else{
+            return res.status(404).json({
+                success:false,
+                message:'Product not found'
+            })
+        }
+    }catch(err){
+        return res.status(500).json({
+            success:false,
+            err:err
         })
     }
 })
